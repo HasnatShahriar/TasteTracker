9606932c65e9fa32fa821561ec965cfec2302154
HasnatShahriar <shahriarhriday94@gmail.com>
2024-05-12 01:29:52 +0600

    implement the search option in all foods page

--- src/pages/AllFood/AllFood.jsx
+++ src/pages/AllFood/AllFood.jsx
@@ -1,16 +1,53 @@
+// import { useLoaderData } from "react-router-dom";
+// import FoodCard from "../../components/FoodCard";
+
+
+// const AllFood = () => {
+//   const foods = useLoaderData();
+//   console.log(foods);
+//   return (
+//     <div>
+//       <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+//         {
+//           foods.map(food => <FoodCard key={food._id} food={food} />)
+//         }
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AllFood;
+
+
+
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import FoodCard from "../../components/FoodCard";
 
-
 const AllFood = () => {
-  const foods = useLoaderData();
-  console.log(foods);
+  const allFoods = useLoaderData();
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Filter food items based on search query
+  const filteredFoods = allFoods.filter(food =>
+    food.foodName.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        {
-          foods.map(food => <FoodCard key={food._id} food={food} />)
-        }
+      {/* Search input field */}
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        placeholder="Search food by name..." className="border-2 p-2"
+      />
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-6">
+        {/* Display filtered food items */}
+        {filteredFoods.map((food) => (
+          <FoodCard key={food._id} food={food} />
+        ))}
       </div>
     </div>
   );
