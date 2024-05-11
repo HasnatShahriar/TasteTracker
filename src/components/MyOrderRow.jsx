0adb96da753158acd8be8800beae1777baae9998
HasnatShahriar <shahriarhriday94@gmail.com>
2024-05-11 18:04:13 +0600

    implemented delete functionality on delete button in my purchase foods page

--- src/components/MyOrderRow.jsx
+++ src/components/MyOrderRow.jsx
@@ -1,20 +1,24 @@
 
 
-const MyOrderRow = ({ food,index }) => {
+
+const MyOrderRow = ({ food, index,handleDelete }) => {
   const { _id, name, price, quantity, buyerName, buyerEmail, date, image, category, addedBy, origin, description } = food
+
+
+
   return (
     <tr>
       <th>
         {/* <label>
           <input type="checkbox" className="checkbox" />
         </label> */}
-        {index+1}
+        {index + 1}
       </th>
       <td>
-          <div className="avatar">
-            <div className="rounded w-24 h-24">
-              <img src={image} alt="Buying Food Image" />
-            </div> 
+        <div className="avatar">
+          <div className="rounded w-24 h-24">
+            <img src={image} alt="Buying Food Image" />
+          </div>
         </div>
       </td>
       <td>
@@ -27,10 +31,12 @@ const MyOrderRow = ({ food,index }) => {
         {date}
       </td>
       <td>{buyerName}</td>
-      <td>${buyerEmail}</td>
+      <td>{buyerEmail}</td>
 
       <th>
-        <button className="btn btn-ghost btn-xs">Delete</button>
+        <button onClick={()=>handleDelete(_id)} className="btn btn-sm btn-square">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+        </button>
       </th>
     </tr>
   );
--- src/pages/MyOrder/MyOrder.jsx
+++ src/pages/MyOrder/MyOrder.jsx
@@ -1,55 +1,88 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../provider/AuthProvider";
 import MyOrderRow from "../../components/MyOrderRow";
+import Swal from "sweetalert2";
 
 
 const MyOrder = () => {
 
   const { user } = useContext(AuthContext);
 
-  const [foods,setFoods] = useState([])
+  const [foods, setFoods] = useState([])
 
   const url = `http://localhost:5000/purchase/${user?.email}`
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      setFoods(data)
-    })
-  },[url])
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setFoods(data)
+      })
+  }, [url])
+
+
+  const handleDelete = id => {
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          fetch(`${import.meta.env.VITE_API_URL}/purchase/${id}`, {
+            method: 'DELETE'
+          })
+            .then(res => res.json())
+            .then(data => {
+              console.log(data);
+              if (data.deletedCount > 0) {
+                Swal.fire({
+                  title: "Deleted!",
+                  text: "Your file has been deleted.",
+                  icon: "success"
+                });
+                const remaining = foods.filter(food => food._id !== id);
+                setFoods(remaining)
+              }
+            })
+        }
+      });
+  }
 
 
   return (
     <div>
       <div className="overflow-x-auto">
-  <table className="table">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>
-          {/* <label>
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                {/* <label>
             <input type="checkbox" className="checkbox" />
           </label> */}
-          Serial
-        </th>
-        <th>Food Image</th>
-        <th>Food Name</th>
-        <th>Price</th>
-        <th>Buying Time</th>
-        <th>Customer Name</th>
-        <th>Customer Email</th>
-        <th>Delete</th>
-      </tr>
-    </thead>
-    <tbody>
-      {
-        foods.map((food,index) => <MyOrderRow key={food._id} food={food} index={index}></MyOrderRow>)
-      }   
-    </tbody>
-  </table>
-</div>
+                Serial
+              </th>
+              <th>Food Image</th>
+              <th>Food Name</th>
+              <th>Price</th>
+              <th>Buying Time</th>
+              <th>Customer Name</th>
+              <th>Customer Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              foods.map((food, index) => <MyOrderRow key={food._id} food={food} index={index} handleDelete={handleDelete}></MyOrderRow>)
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
