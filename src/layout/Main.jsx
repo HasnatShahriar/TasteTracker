b68520c22c00692a64b904dd32cc7c2999cf0d32
HasnatShahriar <shahriarhriday94@gmail.com>
2024-05-12 01:56:58 +0600

    added footer page in all pages

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/Shared/Navbar";
+import Footer from "../pages/Shared/Footer";
 
 
 const Main = () => {
   return (
     <div className="container mx-auto">
-      <Navbar/>
-      <Outlet/>
+      <Navbar />
+      <div className="min-h-[calc(100vh-339px)]">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 };
--- src/pages/AllFood/AllFood.jsx
+++ src/pages/AllFood/AllFood.jsx
@@ -1,25 +1,3 @@
-// import { useLoaderData } from "react-router-dom";
-// import FoodCard from "../../components/FoodCard";
-
-
-// const AllFood = () => {
-//   const foods = useLoaderData();
-//   console.log(foods);
-//   return (
-//     <div>
-//       <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-//         {
-//           foods.map(food => <FoodCard key={food._id} food={food} />)
-//         }
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default AllFood;
-
-
-
 import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import FoodCard from "../../components/FoodCard";
@@ -35,7 +13,6 @@ const AllFood = () => {
 
   return (
     <div>
-      {/* Search input field */}
       <input
         type="text"
         value={searchQuery}
@@ -44,7 +21,7 @@ const AllFood = () => {
       />
 
       <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-6">
-        {/* Display filtered food items */}
+
         {filteredFoods.map((food) => (
           <FoodCard key={food._id} food={food} />
         ))}
