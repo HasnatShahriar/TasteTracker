e385c7a1cc2d8c3304c0b80a0eb959ea7d10ab89
HasnatShahriar <shahriarhriday94@gmail.com>
2024-05-13 00:49:39 +0600

    added the server side url

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD37Q2Z2Qsf_gVigGdCP-1RkQpOGLc2Kf4",
-  authDomain: "taste-tracker-f56b5.firebaseapp.com",
-  projectId: "taste-tracker-f56b5",
-  storageBucket: "taste-tracker-f56b5.appspot.com",
-  messagingSenderId: "21077114690",
-  appId: "1:21077114690:web:e52161ac20d9c0b58f12db"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
