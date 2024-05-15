a90d4ecd954c3e5cbd7251d0618451f04ffd2dea
HasnatShahriar <shahriarhriday94@gmail.com>
2024-05-15 15:19:49 +0600

    added see all foods button in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,19 +3,23 @@ import Banner from "../../components/Banner";
 import TopSelling from "../../components/TopSelling";
 import NewsletterSection from "../../components/NewsletterSection";
 import About from "../../components/About";
+import { Link } from "react-router-dom";
 
 
 const Home = () => {
- 
+
   return (
-    <div>    
+    <div>
       <Helmet>
         <title>TasteTracker | Home</title>
       </Helmet>
-      <Banner/>
-     <TopSelling/>
-     <About/>
-     <NewsletterSection/>
+      <Banner />
+      <TopSelling />
+      <div className="mt-10 text-center">
+        <Link to='/allFoods'> <button className="bg-green-600 hover:bg-green-700 text-white text-lg  px-4 py-2 rounded-md btn  ">See All Foods</button></Link>
+      </div>
+      <About />
+      <NewsletterSection />
     </div>
   );
 };
